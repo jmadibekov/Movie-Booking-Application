6471dc80dec222e761d1453a915055bdaf074ef9
Mohamed Fazli <moha0171@e.ntu.edu.sg>
2019-11-08 22:15:16 +0800

    fazli_updated staff_view

--- src/moblima/view/ChooseShowtime.java
+++ src/moblima/view/ChooseShowtime.java
@@ -5,6 +5,7 @@ import moblima.model.*;
 import moblima.model.StackArg;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChooseShowtime{
 
@@ -22,42 +23,44 @@ public class ChooseShowtime{
 		//function to get showtimes based on cineplex and movie and cinema chosen
 
 		int gotShowtimes = 0;
+		int index = 1;
 		ArrayList< Showtime > showtimesList = BookingController.getShowtimesList();
+		HashMap<Integer, Showtime> uniqueShowtimes = new HashMap<Integer, Showtime>();
 
 		for (Showtime i : showtimesList) {
-			System.out.printf("%s, %s, Type: %s, CinemaClass: %s\n", i.getDate(), i.getTime(), i.getType(),
+			System.out.printf("(%s) %s, %s, Type: %s, CinemaClass: %s\n",index, i.getDate(), i.getTime(), i.getType(),
 					BookingController.getCinemaClass(i.getCinemaId()));
+			uniqueShowtimes.put(index, i);
+			index++;
 			gotShowtimes++;
 		}
 
 		if (gotShowtimes == 0) {
 			System.out.println("No showtimes available. Please try another movie");
+			navigation.goBack();
 		}
 
-		System.out.println("(99) Go back to main menu\n");
 		StackArg curView = navigation.getLastView();
 
 		while (true) {
 			int input = navigation.getChoice("Please select an option: ");
+			boolean found = false;
 			if (input == 0) {
 				navigation.goBack();
 				break;
 			}
-
-			boolean found = false;
-			for (Showtime i : showtimesList) {
-				if (input == -1) {
-					BookingController.setChosenShowtime(i);
-					BookingController.setSeatLayout(i.getSeatLayout());
+			else if (input <= uniqueShowtimes.size()) {
+				if (curView.getUserType() == 1) {
+					BookingController.setChosenShowtime(uniqueShowtimes.get(input));
 					navigation.goTo(new StackArg("chooseSeats", curView.getUserType()));
 					found = true;
+				} else {
+					BookingController.setChosenShowtime(uniqueShowtimes.get(input));
 					break;
 				}
 			}
-
-			if (found == true)
+			if (found)
 				break;
-
 			System.out.println("\nPlease enter a valid input\n");
 		}
 	}
