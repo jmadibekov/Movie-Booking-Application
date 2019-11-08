57485df00661a9f2d978913d6bd4f417e3398c11
Mohamed Fazli <moha0171@e.ntu.edu.sg>
2019-11-08 22:03:50 +0800

    fazli_updated staff_view

--- src/moblima/view/ChooseMovie.java
+++ src/moblima/view/ChooseMovie.java
@@ -4,6 +4,7 @@ import moblima.controller.*;
 import moblima.model.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChooseMovie {
 	public ChooseMovie() {
@@ -21,60 +22,54 @@ public class ChooseMovie {
 		//function to get movie based on cineplex chosen
 
 		int gotMovies = 0;
+		int index = 1;
 		ArrayList < Movie > movieList = BookingController.getMovies();
-
+		HashMap<Integer, Movie> uniqueMovies = new HashMap<Integer, Movie>();
 		for (Movie i: movieList) {
 			if (curView.getUserType() == 1) {
 				if (i.getShowingStatus().contentEquals("NowShowing") || i.getShowingStatus().contentEquals("Preview")) {
 					System.out.printf("(%s) %s, Rating: %s, ShowingStatus: %s\n",
-							i.getMovieId(), i.getTitle(), i.getOverallRating(), i.getShowingStatus());
+							index, i.getTitle(), i.getOverallRating(), i.getShowingStatus());
+					uniqueMovies.put(index, i);
 					gotMovies++;
+					index++;
 				}
 			}
 			else {
 				System.out.printf("(%s) %s, Rating: %s, ShowingStatus: %s\n",
-						i.getMovieId(), i.getTitle(), i.getOverallRating(), i.getShowingStatus());
+						index, i.getTitle(), i.getOverallRating(), i.getShowingStatus());
+				uniqueMovies.put(index,i);
 				gotMovies++;
+				index++;
 			}
 		}
 
 		System.out.print("\n");
 		if (gotMovies == 0) {
-			System.out.println("No movies are currently showing. Please try another cineplex location");
+			System.out.println("No movies are currently showing. Please try another cineplex\n");
+			navigation.goBack();
 		}
 
 		while (true) {
 			int input = navigation.getChoice("Please select an option: ");
+			boolean found = false;
 			if (input == 0) {
 				navigation.goBack();
 				break;
 			}
-
-			boolean found = false;
-			for (Movie i: movieList) {
+			else if (input <= uniqueMovies.size()) {
 				if (curView.getUserType() == 1) {
-					if (Integer.parseInt(i.getMovieId()) == input
-							&& (i.getShowingStatus().contentEquals("NowShowing") || i.getShowingStatus().contentEquals("Preview"))) {
-						BookingController.setChosenMovie(i);
-						navigation.goTo(new StackArg("chooseShowtime", curView.getUserType()));
-						found = true;
-						break;
-					}
-				}
-				else {
-					if (Integer.parseInt(i.getMovieId()) == input) {
-						navigation.goTo(new StackArg(curView.getGoNextView(), curView.getUserType()));
-						found = true;
-						break;
-					}
+					BookingController.setChosenMovie(uniqueMovies.get(input));
+					navigation.goTo(new StackArg("chooseShowtime", curView.getUserType()));
+					found = true;
+				} else {
+					BookingController.setChosenMovie(uniqueMovies.get(input));
+					break;
 				}
 			}
-
-			if (found == true)
+			if (found)
 				break;
-
 			System.out.println("\nPlease enter a valid input\n");
 		}
 	}
-	
 }
